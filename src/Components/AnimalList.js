01ea42d8b36807f37588d4c42059f4564cf42863
Patrick Kille <pr.kille@gmail.com>
2020-05-11 11:07:52 -0700

    add in create form in animal list
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>
    Co-authored-by: Dusty McCord <dustymccord@gmail.com>
    Co-authored-by: Geoff Goetz <59552871+Pieharder@users.noreply.github.com>

--- src/Components/AnimalList.js
+++ src/Components/AnimalList.js
@@ -3,11 +3,25 @@ import PropTypes from 'prop-types';
 import SingleAnimal from './SingleAnimal';
 
 function AnimalList(props) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, breed, age, gender } = e.target;
+    console.log('submit', name.value, breed.value, age.value, gender.value);
+  };
   return (
-    <div>
-      <h1> Animal List </h1>
-      <SingleAnimal />
-    </div>
+    <React.Fragment>
+      <div>
+        <h1> Animal List </h1>
+        <SingleAnimal />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" />
+        <input type="text" name="breed" placeholder="Breed" />
+        <input type="text" name="age" placeholder="Age" />
+        <input type="text" name="gender" placeholder="Gender" />
+        <button type="submit">Add Animal</button>
+      </form>
+    </React.Fragment>
   );
 }
 
